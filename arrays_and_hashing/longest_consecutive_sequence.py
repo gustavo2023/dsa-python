1badefe4fcee60e9108cb3cc23229522dd7757e0
gustavo2023 <gustavoegutierrezg23@gmail.com>
2026-02-06 16:03:56 -0400

    Refactor solution for "Longest Consecutive Sequence" problem to eliminate unnecessary set conversion

--- arrays_and_hashing/longest_consecutive_sequence.py
+++ arrays_and_hashing/longest_consecutive_sequence.py
@@ -17,17 +17,16 @@ class Solution:
         if len(nums) == 0:
             return 0
 
-        nums_set = set(nums)
         longest = 0
 
-        for n in nums_set:
-            if n - 1 not in nums_set:
-                curr_length = 0
+        for n in nums:
+            if n - 1 not in nums:
+                count = 0
 
-                while n + curr_length in nums_set:
-                    curr_length += 1
+                while n + count in nums:
+                    count += 1
 
-                longest = max(longest, curr_length)
+                longest = max(longest, count)
 
         return longest
 
